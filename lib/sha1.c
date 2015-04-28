6d3c17d5c282ca3d765a005602ad4639357771b7
Pavel Tvrdík <pawel.tvrdik@gmail.cz>
2015-04-28 09:28:49 +0200

    SHA1: Integrate sha1_hmac.c into sha1.c

--- lib/sha1.c
+++ lib/sha1.c
@@ -280,3 +280,61 @@ sha1_hash_buffer(byte *outbuf, const byte *buffer, uint length)
   sha1_update(&hd, buffer, length);
   memcpy(outbuf, sha1_final(&hd), SHA1_SIZE);
 }
+
+void
+sha1_hmac_init(sha1_hmac_context *hd, const byte *key, uint keylen)
+{
+  byte keybuf[SHA1_BLOCK_SIZE], buf[SHA1_BLOCK_SIZE];
+
+  // Hash the key if necessary
+  if (keylen <= SHA1_BLOCK_SIZE)
+  {
+    memcpy(keybuf, key, keylen);
+    bzero(keybuf + keylen, SHA1_BLOCK_SIZE - keylen);
+  }
+  else
+  {
+    sha1_hash_buffer(keybuf, key, keylen);
+    bzero(keybuf + SHA1_SIZE, SHA1_BLOCK_SIZE - SHA1_SIZE);
+  }
+
+  // Initialize the inner digest
+  sha1_init(&hd->ictx);
+  int i;
+  for (i = 0; i < SHA1_BLOCK_SIZE; i++)
+    buf[i] = keybuf[i] ^ 0x36;
+  sha1_update(&hd->ictx, buf, SHA1_BLOCK_SIZE);
+
+  // Initialize the outer digest
+  sha1_init(&hd->octx);
+  for (i = 0; i < SHA1_BLOCK_SIZE; i++)
+    buf[i] = keybuf[i] ^ 0x5c;
+  sha1_update(&hd->octx, buf, SHA1_BLOCK_SIZE);
+}
+
+void
+sha1_hmac_update(sha1_hmac_context *hd, const byte *data, uint datalen)
+{
+  // Just update the inner digest
+  sha1_update(&hd->ictx, data, datalen);
+}
+
+byte *sha1_hmac_final(sha1_hmac_context *hd)
+{
+  // Finish the inner digest
+  byte *isha = sha1_final(&hd->ictx);
+
+  // Finish the outer digest
+  sha1_update(&hd->octx, isha, SHA1_SIZE);
+  return sha1_final(&hd->octx);
+}
+
+void
+sha1_hmac(byte *outbuf, const byte *key, uint keylen, const byte *data, uint datalen)
+{
+  sha1_hmac_context hd;
+  sha1_hmac_init(&hd, key, keylen);
+  sha1_hmac_update(&hd, data, datalen);
+  byte *osha = sha1_hmac_final(&hd);
+  memcpy(outbuf, osha, SHA1_SIZE);
+}
