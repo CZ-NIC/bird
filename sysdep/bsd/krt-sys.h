10e1e8144d57f7dd569e1f7c6183e005b4338882
Pavel Tvrdík <pawel.tvrdik@gmail.com>
2015-11-02 12:34:49 +0100

    Return GA_UNKNOWN in krt_sys_get_attr() at BSD*
    Thanks to Ondrej 'Santiago' Zajicek

--- sysdep/bsd/krt-sys.h
+++ sysdep/bsd/krt-sys.h
@@ -44,7 +44,7 @@ struct krt_state {
 
 static inline void krt_sys_init(struct krt_proto *p UNUSED) { }
 
-static inline int krt_sys_get_attr(eattr *a UNUSED, byte *buf UNUSED, int buflen UNUSED) { return 0; }
+static inline int krt_sys_get_attr(eattr *a UNUSED, byte *buf UNUSED, int buflen UNUSED) { return GA_UNKNOWN; }
 
 
 #endif
