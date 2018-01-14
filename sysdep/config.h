4db4ac7243bf54187029abda0b42cc9d29757d13
Ondrej Zajicek (work) <santiago@crfreenet.org>
2018-01-14 21:52:58 +0100

    NEWS and version update

--- sysdep/config.h
+++ sysdep/config.h
@@ -13,7 +13,7 @@
 #ifdef GIT_LABEL
 #define BIRD_VERSION XSTR1(GIT_LABEL)
 #else
-#define BIRD_VERSION "2.0.0"
+#define BIRD_VERSION "2.0.1"
 #endif
 
 /* Include parameters determined by configure script */
