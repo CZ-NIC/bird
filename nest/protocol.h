73b9b42a5aa51518b4dad04f3e2662e951faa079
Maria Matějka <mq@jmq.cz>
2018-10-26 09:32:35 +0200

    Perf: Protocol to measure BIRD performance internally

--- nest/protocol.h
+++ nest/protocol.h
@@ -47,6 +47,7 @@ enum protocol_class {
   PROTOCOL_KERNEL,
   PROTOCOL_OSPF,
   PROTOCOL_MRT,
+  PROTOCOL_PERF,
   PROTOCOL_PIPE,
   PROTOCOL_RADV,
   PROTOCOL_RIP,
@@ -100,7 +101,8 @@ void protos_dump_all(void);
 
 extern struct protocol
   proto_device, proto_radv, proto_rip, proto_static, proto_mrt,
-  proto_ospf, proto_pipe, proto_bgp, proto_bfd, proto_babel, proto_rpki;
+  proto_ospf, proto_perf,
+  proto_pipe, proto_bgp, proto_bfd, proto_babel, proto_rpki;
 
 /*
  *	Routing Protocol Instance
