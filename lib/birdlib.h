b835f396346491524b86b96f88ce2dbfb1a96ff1
Maria Matejka <mq@ucw.cz>
2019-01-23 17:08:27 +0100

    GDB pretty printers: f_inst and f_val.

--- lib/birdlib.h
+++ lib/birdlib.h
@@ -157,6 +157,15 @@ void debug(const char *msg, ...);	/* Printf to debug output */
 #define ASSERT(x) do { if (!(x)) log(L_BUG "Assertion '%s' failed at %s:%d", #x, __FILE__, __LINE__); } while(0)
 #endif
 
+#ifdef DEBUGGING
+asm(
+    ".pushsection \".debug_gdb_scripts\", \"MS\",@progbits,1\n"
+    ".byte 1\n" /* Python */
+    ".asciz \"bird-gdb.py\"\n"
+    ".popsection\n"
+   );
+#endif
+
 /* Pseudorandom numbers */
 
 u32 random_u32(void);
