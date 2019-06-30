bb57d9171f2b4567f54169c8864953c4e5e18025
Ondrej Zajicek (work) <santiago@crfreenet.org>
2019-06-30 22:30:56 +0200

    Add mock-up function for unit tests
    They failed without it.

--- test/birdtest.c
+++ test/birdtest.c
@@ -487,6 +487,7 @@ void cmd_reconfig(char *name UNUSED, int type UNUSED, int timeout UNUSED) {}
 void cmd_reconfig_confirm(void) {}
 void cmd_reconfig_undo(void) {}
 void cmd_reconfig_status(void) {}
+void cmd_graceful_restart(void) {}
 void cmd_shutdown(void) {}
 void cmd_reconfig_undo_notify(void) {}
 
