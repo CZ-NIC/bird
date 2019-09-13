d5311f1396328a9c2e8dbe8c05a59e2a5832e126
Maria Matejka <mq@ucw.cz>
2019-09-13 14:47:46 +0200

    Worker test

---
+++ sysdep/unix/worker_test.c
@@ -0,0 +1,80 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#define TEST_MAX (1 << 20)
+#define THREAD_NUM  42
+
+#include "test/birdtest.h"
+#include "test/bt-utils.h"
+
+#include "lib/worker.h"
+#include "conf/conf.h"
+
+#include <stdatomic.h>
+
+struct t_rwlock_task {
+  struct task task;
+  struct domain *domain;
+  _Atomic uint *total_counter;
+  _Atomic uint *allocated;
+};
+
+static void t_rwlock_execute(struct task *task)
+{
+  struct t_rwlock_task *t = SKIP_BACK(struct t_rwlock_task, task, task);
+  domain_read_lock(t->domain);
+  uint tot = atomic_fetch_add(t->total_counter, 1);
+  bt_log("Total counter: %u\n", tot);
+  uint prev = atomic_fetch_sub(t->allocated, 1);
+  bt_log("Prev allocated: %u\n", prev);
+  bt_assert(prev > 0 && prev <= THREAD_NUM+1);
+  domain_read_unlock(t->domain);
+  xfree(t);
+}
+
+static int
+t_rwlock_read(void)
+{
+  struct config conf = { .workers = THREAD_NUM };
+
+  worker_queue_init();
+  worker_queue_update(&conf);
+
+  struct domain *domain = domain_new(&root_pool);
+  _Atomic uint total_counter = 0;
+  _Atomic uint allocated = 0;
+  for (int i=0; i<TEST_MAX; i++)
+  {
+    atomic_fetch_add(&allocated, 1);
+    struct t_rwlock_task *t = xmalloc(sizeof(struct t_rwlock_task));
+    *t = (struct t_rwlock_task) {
+      .task = {
+	.execute = t_rwlock_execute,
+      },
+      .domain = domain,
+      .total_counter = &total_counter,
+      .allocated = &allocated,
+    };
+    bt_log("Task pushed (before)\n");
+    task_push(&t->task);
+    bt_log("Task pushed (after)\n");
+  }
+
+  conf.workers = 0;
+  worker_queue_update(&conf);
+
+  bt_log("Returning 1\n");
+  return 1;
+}
+
+int main(int argc, char *argv[])
+{
+  bt_init(argc, argv);
+  bt_bird_init();
+
+  bt_test_suite(t_rwlock_read, "rwlock concurrent read");
+
+  return bt_exit_value();
+}
+
