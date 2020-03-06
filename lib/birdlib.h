d53623f120b7b5b3d4fc757411acca51d560544c
Maria Matejka <mq@jmq.cz>
2020-03-06 15:41:51 +0100

    Route export: dropped tail-execution of do_rt_notify()

--- lib/birdlib.h
+++ lib/birdlib.h
@@ -73,6 +73,7 @@ static inline int u64_cmp(u64 i1, u64 i2)
 #define UNUSED __attribute__((unused))
 #define PACKED __attribute__((packed))
 #define NONNULL(...) __attribute__((nonnull((__VA_ARGS__))))
+#define USE_RESULT __attribute__((warn_unused_result))
 
 #ifndef HAVE_THREAD_LOCAL
 #define _Thread_local
