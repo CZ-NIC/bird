52b6e352512b1cee627854e1ab9619f87f3622e1
Maria Matejka <mq@ucw.cz>
2020-03-10 16:13:41 +0100

    Route export: Common export function.
    Also:
    * Feeding request by net.
    * Passing only net_addr in export structure.
    * Cleanup of krt_replace_rte() call.

--- sysdep/linux/netlink.c
+++ sysdep/linux/netlink.c
@@ -1394,7 +1394,7 @@ nl_replace_rte(struct krt_proto *p, rte *e)
 
 
 void
-krt_replace_rte(struct krt_proto *p, net *n UNUSED, rte *new, rte *old)
+krt_replace_rte(struct krt_proto *p, rte *new, rte *old)
 {
   int err = 0;
 
