d114959e3aeef872441dccea34552047380af742
Ondrej Zajicek (work) <santiago@crfreenet.org>
2021-05-14 18:44:52 +0200

    Flowspec: Fix values for true/false operators
    RFC 8955 is pretty clear that 000 is false and 111 is true.

--- lib/flowspec.h
+++ lib/flowspec.h
@@ -15,14 +15,14 @@
 
 
 /* Flow component operators */
-#define FLOW_OP_TRUE		0x00	/* 0b000 */
+#define FLOW_OP_FALSE		0x00	/* 0b000 */
 #define FLOW_OP_EQ		0x01	/* 0b001 */
 #define FLOW_OP_GT		0x02	/* 0b010 */
 #define FLOW_OP_GEQ		0x03	/* 0b011 */
 #define FLOW_OP_LT		0x04	/* 0b100 */
 #define FLOW_OP_LEQ		0x05	/* 0b101 */
 #define FLOW_OP_NEQ		0x06	/* 0b110 */
-#define FLOW_OP_FALSE		0x07	/* 0b111 */
+#define FLOW_OP_TRUE		0x07	/* 0b111 */
 
 #define FLOW_OP_OR		0x00
 #define FLOW_OP_AND		0x40
