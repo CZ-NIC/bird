71b3456eede17be6646b7deebff84f34ee5755f7
Maria Matejka <mq@ucw.cz>
2021-10-22 19:43:55 +0200

    Better profylaction recursive route loops
    In some specific configurations, it was possible to send BIRD into an
    infinite loop of recursive next hop resolution. This was caused by route
    priority inversion.
    
    To prevent priority inversions affecting other next hops, we simply
    refuse to resolve any next hop if the best route for the matching prefix
    is recursive or any other route with the same preference is recursive.
    
    Next hop resolution doesn't change route priority, therefore it is
    perfectly OK to resolve BGP next hops e.g. by an OSPF route, yet if the
    same (or covering) prefix is also announced by iBGP, by retraction of
    the OSPF route we would get a possible priority inversion.

--- nest/rt-table.c
+++ nest/rt-table.c
@@ -3529,15 +3529,18 @@ rt_update_hostentry(rtable *tab, struct hostentry *he)
     {
       rte *e = n->routes;
       rta *a = e->attrs;
-      pxlen = n->n.addr->pxlen;
+      word pref = a->pref;
 
-      if (a->hostentry)
+      for (rte *ee = n->routes; ee; ee = ee->next)
+	if ((ee->attrs->pref >= pref) && ee->attrs->hostentry)
 	{
 	  /* Recursive route should not depend on another recursive route */
 	  log(L_WARN "Next hop address %I resolvable through recursive route for %N",
 	      he->addr, n->n.addr);
 	  goto done;
 	}
+
+      pxlen = n->n.addr->pxlen;
 
       if (a->dest == RTD_UNICAST)
 	{
