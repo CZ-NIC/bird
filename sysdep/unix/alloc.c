CZ-NIC/bird

commit 38278d94ba0a179d5eeb061a59850a4e1c150e5b

No memory unmapping when shutting down
All the memory is just freed implicitly on exit, no need for
page-by-page unmapping.

sysdep/unix/alloc.c
@@ -46,6 +46,8 @@ alloc_sys_page(void)
   return ptr;
 }
 
+extern int shutting_down; /* Shutdown requested. */
+
 void *
 alloc_page(void)
 {
@@ -58,7 +60,7 @@ alloc_page(void)
     
     node *n = HEAD(fp->list);
     rem_node(n);
-    if (--fp->cnt < fp->min)
+    if (!shutting_down && (--fp->cnt < fp->min))
       ev_send(&global_work_list, fp->cleanup);
 
     void *ptr = n - FP_NODE_OFFSET;
@@ -87,7 +89,7 @@ free_page(void *ptr)
 
     memset(n, 0, sizeof(node));
     add_tail(&fp->list, n);
-    if (++fp->cnt > fp->max)
+    if (!shutting_down && (++fp->cnt > fp->max))
       ev_send(&global_work_list, fp->cleanup);
   }
   else
@@ -151,7 +153,7 @@ cleanup_pages(void *data)
 
   birdloop_leave(loop);
 
-  if (GFP->cnt > GFP->max)
+  if (!shutting_down && (GFP->cnt > GFP->max))
     ev_send(&global_work_list, GFP->cleanup);
 }
 

commit f9e098c98af98096c8f47a054b57cff63d345074

Fixed standby memory page counters on shutdown
Bug introduced by commit 38278d94ba0a179d5eeb061a59850a4e1c150e5b.

sysdep/unix/alloc.c
@@ -60,7 +60,7 @@ alloc_page(void)
     
     node *n = HEAD(fp->list);
     rem_node(n);
-    if (!shutting_down && (--fp->cnt < fp->min))
+    if ((--fp->cnt < fp->min) && !shutting_down)
       ev_send(&global_work_list, fp->cleanup);
 
     void *ptr = n - FP_NODE_OFFSET;
@@ -89,7 +89,7 @@ free_page(void *ptr)
 
     memset(n, 0, sizeof(node));
     add_tail(&fp->list, n);
-    if (!shutting_down && (++fp->cnt > fp->max))
+    if ((++fp->cnt > fp->max) && !shutting_down)
       ev_send(&global_work_list, fp->cleanup);
   }
   else