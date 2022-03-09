c20506dc07e1b99cf1d9f2cf9d00e9211cd9e2f4
Maria Matejka <mq@ucw.cz>
2022-03-09 09:13:21 +0100

    Revert "fixup! Bound allocated pages to resource pools with page caches to avoid unnecessary syscalls"
    This reverts commit bea582cbb53e30dd32a5b6829c7443e0e5558d11.

--- lib/slab.c
+++ lib/slab.c
@@ -178,7 +178,7 @@ struct sl_alignment {			/* Magic structure for testing of alignment */
   int x[0];
 };
 
-#define SL_GET_HEAD(x)	((struct sl_head *) (((uintptr_t) (x)) & ~(page_size-1)))
+#define SL_GET_HEAD(x)	((struct sl_head *) (((uintptr_t) (x)) & ~(get_page_size()-1)))
 
 /**
  * sl_new - create a new Slab
