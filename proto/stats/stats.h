b5ceb9a11c2bfafc8a6145a2ca65feb9c9cf391e
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2022-07-14 14:51:03 +0200

    Remove old pipe protocol code. Add init function.

--- proto/stats/stats.h
+++ proto/stats/stats.h
@@ -1,7 +1,8 @@
 /*
  *	BIRD -- Table-to-Table Routing Protocol a.k.a Pipe
  *
- *	(c) 1999 Martin Mares <mj@ucw.cz>
+ *      (c) 2022 Vojtech Vilimek <vojtech.vilimek@nic.cz>
+ *      (c) 2022 CZ.NIC z.s.p.o.
  *
  *	Can be freely distributed and used under the terms of the GNU GPL.
  */
@@ -11,15 +12,13 @@
 
 struct stats_config {
   struct proto_config c;
-  struct rtable_config *peer;		/* Table we're connected to */
   const net_addr *in_subprefix;
   u8 max_generation;
 };
 
 struct stats_proto {
   struct proto p;
-  struct channel *pri;
-  struct channel *sec;
+  struct channel *c;
   struct tbf rl_gen;
 };
 
