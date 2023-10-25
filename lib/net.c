b5c040d0cfe339478656b494f01c02a589fcf2f6
Ondrej Zajicek <santiago@crfreenet.org>
2023-10-25 19:16:40 +0200

    Nest: Ethernet net type - preliminary support

--- lib/net.c
+++ lib/net.c
@@ -15,6 +15,7 @@ const char * const net_label[] = {
   [NET_FLOW4]	= "flow4",
   [NET_FLOW6]	= "flow6",
   [NET_IP6_SADR]= "ipv6-sadr",
+  [NET_ETH]	= "eth",
   [NET_MPLS]	= "mpls",
   [NET_EVPN]	= "evpn",
 };
@@ -29,6 +30,7 @@ const u16 net_addr_length[] = {
   [NET_FLOW4]	= 0,
   [NET_FLOW6]	= 0,
   [NET_IP6_SADR]= sizeof(net_addr_ip6_sadr),
+  [NET_ETH]	= sizeof(net_addr_eth),
   [NET_MPLS]	= sizeof(net_addr_mpls),
   [NET_EVPN]	= 0,
 };
@@ -43,6 +45,7 @@ const u8 net_max_prefix_length[] = {
   [NET_FLOW4]	= IP4_MAX_PREFIX_LENGTH,
   [NET_FLOW6]	= IP6_MAX_PREFIX_LENGTH,
   [NET_IP6_SADR]= IP6_MAX_PREFIX_LENGTH,
+  [NET_ETH]	= 0,
   [NET_MPLS]	= 0,
   [NET_EVPN]	= 0,
 };
@@ -57,6 +60,7 @@ const u16 net_max_text_length[] = {
   [NET_FLOW4]	= 0,	/* "flow4 { ... }" */
   [NET_FLOW6]	= 0,	/* "flow6 { ... }" */
   [NET_IP6_SADR]= 92,	/* "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff/128 from ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff/128" */
+  [NET_ETH]	= 28,	/* "11:22:33:44:55:66 vlan 65535" */
   [NET_MPLS]	= 7,	/* "1048575" */
   [NET_EVPN]	= 0,
 };
@@ -72,6 +76,7 @@ STATIC_ASSERT(sizeof(net_addr_roa6)	== 28);
 STATIC_ASSERT(sizeof(net_addr_flow4)	==  8);
 STATIC_ASSERT(sizeof(net_addr_flow6)	== 20);
 STATIC_ASSERT(sizeof(net_addr_ip6_sadr)	== 40);
+STATIC_ASSERT(sizeof(net_addr_eth)	== 12);
 STATIC_ASSERT(sizeof(net_addr_mpls)	==  8);
 
 STATIC_ASSERT(sizeof(net_addr_evpn_ead)		== 32);
@@ -132,6 +137,16 @@ net_format(const net_addr *N, char *buf, int buflen)
     return flow6_net_format(buf, buflen, &n->flow6);
   case NET_IP6_SADR:
     return bsnprintf(buf, buflen, "%I6/%d from %I6/%d", n->ip6_sadr.dst_prefix, n->ip6_sadr.dst_pxlen, n->ip6_sadr.src_prefix, n->ip6_sadr.src_pxlen);
+  case NET_ETH:
+  {
+    int c = bsnprintf(buf, buflen, "%6b", &n->eth.mac);
+    ADVANCE(buf, buflen, c);
+
+    if (n->eth.vid)
+      c += bsnprintf(buf, buflen, " vlan %d", (int) n->eth.vid);
+
+    return c;
+  }
   case NET_MPLS:
     return bsnprintf(buf, buflen, "%u", n->mpls.label);
   case NET_EVPN:
@@ -159,6 +174,7 @@ net_pxmask(const net_addr *a)
   case NET_IP6_SADR:
     return ipa_from_ip6(ip6_mkmask(net6_pxlen(a)));
 
+  case NET_ETH:
   case NET_MPLS:
   case NET_EVPN:
   default:
@@ -192,6 +208,8 @@ net_compare(const net_addr *a, const net_addr *b)
     return net_compare_flow6((const net_addr_flow6 *) a, (const net_addr_flow6 *) b);
   case NET_IP6_SADR:
     return net_compare_ip6_sadr((const net_addr_ip6_sadr *) a, (const net_addr_ip6_sadr *) b);
+  case NET_ETH:
+    return net_compare_eth((const net_addr_eth *) a, (const net_addr_eth *) b);
   case NET_MPLS:
     return net_compare_mpls((const net_addr_mpls *) a, (const net_addr_mpls *) b);
   case NET_EVPN:
@@ -216,6 +234,7 @@ net_hash(const net_addr *n)
   case NET_FLOW4: return NET_HASH(n, flow4);
   case NET_FLOW6: return NET_HASH(n, flow6);
   case NET_IP6_SADR: return NET_HASH(n, ip6_sadr);
+  case NET_ETH: return NET_HASH(n, eth);
   case NET_MPLS: return NET_HASH(n, mpls);
   case NET_EVPN: return NET_HASH(n, evpn);
   default: bug("invalid type");
@@ -239,6 +258,7 @@ net_validate(const net_addr *n)
   case NET_FLOW4: return NET_VALIDATE(n, flow4);
   case NET_FLOW6: return NET_VALIDATE(n, flow6);
   case NET_IP6_SADR: return NET_VALIDATE(n, ip6_sadr);
+  case NET_ETH: return NET_VALIDATE(n, eth);
   case NET_MPLS: return NET_VALIDATE(n, mpls);
   case NET_EVPN: return NET_VALIDATE(n, evpn);
   default: return 0;
@@ -267,6 +287,7 @@ net_normalize(net_addr *N)
   case NET_IP6_SADR:
     return net_normalize_ip6_sadr(&n->ip6_sadr);
 
+  case NET_ETH:
   case NET_MPLS:
   case NET_EVPN:
     return;
@@ -295,6 +316,7 @@ net_classify(const net_addr *N)
   case NET_IP6_SADR:
     return ip6_zero(n->ip6_sadr.dst_prefix) ? (IADDR_HOST | SCOPE_UNIVERSE) : ip6_classify(&n->ip6_sadr.dst_prefix);
 
+  case NET_ETH:
   case NET_MPLS:
   case NET_EVPN:	/* ?? */
     return IADDR_HOST | SCOPE_UNIVERSE;
@@ -329,6 +351,7 @@ ipa_in_netX(const ip_addr a, const net_addr *n)
     return ip6_zero(ip6_and(ip6_xor(ipa_to_ip6(a), net6_prefix(n)),
 			    ip6_mkmask(net6_pxlen(n))));
 
+  case NET_ETH:
   case NET_MPLS:
   case NET_EVPN:
   default:
