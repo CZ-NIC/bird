224fbe59802ea45820ce49fd1f59b91314b17e7e
Katerina Kubecova <katerina.kubecova@nic.cz>
2023-11-08 13:46:42 +0100

    ASPA: Automatic channel reload

--- nest/proto.c
+++ nest/proto.c
@@ -422,6 +422,11 @@ channel_roa_subscribe_filter(struct channel *c, int dir)
       found = 1;
       break;
 
+    case FI_ASPA_CHECK_EXPLICIT:
+      tab = fi->i_FI_ASPA_CHECK_EXPLICIT.rtc->table;
+      if (valid) channel_roa_subscribe(c, tab, dir);
+      found = 1;
+      break;
     default:
       break;
     }
