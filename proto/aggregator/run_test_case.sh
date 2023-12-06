bd7248af78c26f8d690533d48bf9d09fadee3521
Igor Putovny <igor.putovny@nic.cz>
2023-12-06 10:44:15 +0100

    Add small changes to test run script

--- proto/aggregator/run_test_case.sh
+++ proto/aggregator/run_test_case.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir_name="trie-test-case"
+dir_name="aggregator-test-case"
 
 cd ../..
 make all
@@ -8,7 +8,7 @@ echo '------------------------'
 
 if [[ ! -e "$dir_name" ]]; then
     mkdir "$dir_name"
-    echo "creating directory '$dir_name'"
+    echo "creating directory 'bird/$dir_name'"
 fi
 
 cp ./bird ./"$dir_name"
@@ -24,4 +24,7 @@ cd "$dir_name"
 
 echo 'done'
 echo
-echo 'expected result: 10.100.0.0/16'
+echo 'expected result:'
+echo '====THIRD PASS===='
+echo '0.0.0.0/0'
+echo '10.100.0.0/16'
