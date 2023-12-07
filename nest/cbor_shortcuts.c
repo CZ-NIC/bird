3d80994943a18f42d9c19c73eb55dc37be2332e9
Katerina Kubecova <katerina.kubecova@nic.cz>
2023-12-07 10:28:44 +0100

    cbor ip addresses stored with 52/54 tag, but without yang changes

--- nest/cbor_shortcuts.c
+++ nest/cbor_shortcuts.c
@@ -19,6 +19,16 @@ void cbor_string_uint(struct cbor_writer *writer, char *key, u64 value) {
   cbor_add_uint(writer, value);
 }
 
+void cbor_string_ipv4(struct cbor_writer *writer, char *key, u32 value) {
+  cbor_add_string(writer, key);
+  cbor_add_ipv4(writer, value);
+}
+
+void cbor_string_ipv6(struct cbor_writer *writer, char *key, u64 value) {
+  cbor_add_string(writer, key);
+  cbor_add_ipv6(writer, value);
+}
+
 void cbor_named_block_two_ints(struct cbor_writer *writer, char *key, char *name1, int val1, char *name2, int val2) {
   cbor_add_string(writer, key);
   cbor_open_block_with_length(writer, 2);
@@ -37,3 +47,25 @@ void cbor_write_to_file(struct cbor_writer *writer, char *filename) {
   fclose(write_ptr);
 }
 
+void cbor_add_net(struct cbor_writer *writer, const net_addr *N) {
+  // Original switch comes from lib/net.c and contains more cases.
+  net_addr_union *n = (void *) N;
+
+  switch (n->n.type)
+  {
+  case NET_IP4:
+    cbor_add_ipv4_prefix(writer, n->ip4.prefix, n->ip4.pxlen);
+    return;
+  case NET_IP6:
+    cbor_add_ipv6_prefix(writer, n->ip6.prefix, n->ip6.pxlen);
+    return;
+  case NET_VPN4:
+    cbor_add_ipv4_prefix(writer, n->vpn4.prefix, n->vpn4.pxlen);
+    return;
+  case NET_VPN6:
+    cbor_add_ipv6_prefix(writer, n->vpn6.prefix, n->vpn6.pxlen);
+    return;
+  default:
+    bug("net type unsupported by cbor (yet)."); 
+  }
+}
