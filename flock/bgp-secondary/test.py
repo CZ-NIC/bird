6a52780652f8bc7c9638dfc7b7e5cf567278f0bb
Maria Matejka <mq@ucw.cz>
2024-07-25 17:18:38 +0200

    Flock tests: bgp-secondary data update

--- flock/bgp-secondary/test.py
+++ flock/bgp-secondary/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import asyncio
-from python.BIRD.Test import Test, BIRDInstance
+from python.BIRD.Test import Test, BIRDInstance, DumpRIB
 
 class ThisTest(Test):
     async def prepare(self):
@@ -19,7 +19,7 @@ class ThisTest(Test):
 
     async def test(self):
         # Startup check
-        await self.route_dump(10, "startup")
+        await DumpRIB(self, 10, "startup")()
 
         wtb = ["p170", "p180", "p190", "p200"]
         btw = [*reversed(wtb)]
@@ -27,26 +27,26 @@ class ThisTest(Test):
         # Enable worst to best
         for p in wtb:
             await self.src.enable(p)
-            await self.route_dump(1, f"enable-{p}")
+            await DumpRIB(self, 1, f"enable-{p}")()
 
         # Disable worst to best
         for p in wtb:
             await self.src.disable(p)
-            await self.route_dump(1, f"disable-{p}")
+            await DumpRIB(self, 1, f"disable-{p}")()
 
         # Enable best to worst
         for p in btw:
             await self.src.enable(p)
-            await self.route_dump(1, f"enable-{p}")
+            await DumpRIB(self, 1, f"enable-{p}")()
 
         # Disable best to worst
         for p in btw:
             await self.src.disable(p)
-            await self.route_dump(1, f"disable-{p}")
+            await DumpRIB(self, 1, f"disable-{p}")()
 
         # Re-enable all at once
         await asyncio.gather(*[ self.src.enable(p) for p in wtb ])
-        await self.route_dump(5, f"add-all")
+        await DumpRIB(self, 5, f"add-all")()
 
         # Update configuration
         self.epoch = 1
@@ -56,27 +56,27 @@ class ThisTest(Test):
             f"<INFO> Reloading channel LINK.ipv6",
             f"<INFO> Reconfigured$",
             ])
-        await self.route_dump(5, f"check-reconfig")
+        await DumpRIB(self, 5, f"check-reconfig")()
 
         # Disable worst to best
         for p in wtb:
             await self.src.disable(p)
-            await self.route_dump(1, f"disable-{p}")
+            await DumpRIB(self, 1, f"disable-{p}")()
 
         # Enable best to worst
         for p in btw:
             await self.src.enable(p)
-            await self.route_dump(1, f"enable-{p}")
+            await DumpRIB(self, 1, f"enable-{p}")()
 
         # Disable best to worst
         for p in btw:
             await self.src.disable(p)
-            await self.route_dump(1, f"disable-{p}")
+            await DumpRIB(self, 1, f"disable-{p}")()
 
         # Enable worst to best
         for p in wtb:
             await self.src.enable(p)
-            await self.route_dump(1, f"enable-{p}")
+            await DumpRIB(self, 1, f"enable-{p}")()
 
         # Update configuration once again
         self.epoch = 2
@@ -86,27 +86,27 @@ class ThisTest(Test):
             f"<INFO> Reloading channel LINK.ipv6",
             f"<INFO> Reconfigured$",
             ])
-        await self.route_dump(5, f"check-reconfig")
+        await DumpRIB(self, 5, f"check-reconfig")()
 
         # Disable best to worst
         for p in btw:
             await self.src.disable(p)
-            await self.route_dump(1, f"disable-{p}")
+            await DumpRIB(self, 1, f"disable-{p}")()
 
         # Enable best to worst
         for p in btw:
             await self.src.enable(p)
-            await self.route_dump(1, f"enable-{p}")
+            await DumpRIB(self, 1, f"enable-{p}")()
 
         # Disable worst to best
         for p in wtb:
             await self.src.disable(p)
-            await self.route_dump(1, f"disable-{p}")
+            await DumpRIB(self, 1, f"disable-{p}")()
 
         # Enable worst to best
         for p in wtb:
             await self.src.enable(p)
-            await self.route_dump(1, f"enable-{p}")
+            await DumpRIB(self, 1, f"enable-{p}")()
 
         # Pre-cleanup log checker
         self.src.default_log_checker.append(f"{self.src.logprefix} <RMT> LINK: Received: Administrative shutdown")
