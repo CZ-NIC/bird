8a3c90a217b30b9f02fc89c9acf36d0afea6aebb
Igor Putovny <igor.putovny@nic.cz>
2024-09-26 19:29:02 +0200

    Add selected bucket and FIB status to node

--- proto/aggregator/aggregator.h
+++ proto/aggregator/aggregator.h
@@ -113,12 +113,19 @@ struct aggr_item_node {
   struct aggr_item i;
 };
 
+enum fib_status {
+  IN_FIB = 1,
+  NON_FIB = 2,
+};
+
 struct trie_node {
   struct trie_node *parent;
   struct trie_node *child[2];
   struct trie_node *ancestor;
   struct aggregator_bucket *bucket;
+  struct aggregator_bucket *selected_bucket;
   u32 potential_buckets[POTENTIAL_BUCKETS_BITMAP_SIZE];
+  enum fib_status status;
   int potential_buckets_count;
   int depth;
 };
