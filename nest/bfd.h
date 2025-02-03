665456b6a1095f59ce670ddd91755cffe5f4b75f
Maria Matejka <mq@ucw.cz>
2025-02-03 15:21:52 +0100

    BFD session handling rework
    The original implementation for BIRD 3 was rooted in the first
    methods how I tried to go for multithreading and it had several flaws,
    mostly incomprehensive notification and request pickup routines.
    Also converting to a double-loop architecture where one of the
    loops (low-latency) solely runs BFD socket communication, whereas
    the other one does all the other shenanigans.

--- nest/bfd.h
+++ nest/bfd.h
@@ -7,7 +7,7 @@
 #ifndef _BIRD_NBFD_H_
 #define _BIRD_NBFD_H_
 
-#include "lib/lists.h"
+#include "lib/tlists.h"
 #include "lib/resource.h"
 #include "conf/conf.h"
 
@@ -28,9 +28,21 @@ struct bfd_options {
   list *passwords;			/* Passwords for authentication */
 };
 
-struct bfd_request {
+#define TLIST_PREFIX bfd_request
+#define TLIST_TYPE struct bfd_request
+#define TLIST_ITEM n
+#define TLIST_WANT_ADD_TAIL
+
+/* Reference held by the requestor. Free to unrequest */
+struct bfd_request_ref {
   resource r;
-  node n;
+  struct bfd_request *req;
+};
+
+/* The actual request, allocated by BFD, freed after unrequesting
+ * but assuring safe handling by the low-latency BFD routines. */
+struct bfd_request {
+  TLIST_DEFAULT_NODE;
 
   ip_addr addr;
   ip_addr local;
@@ -38,17 +50,23 @@ struct bfd_request {
   struct iface *vrf;
   struct bfd_options opts;
 
-  void (*hook)(struct bfd_request *);
-  void *data;
-  struct birdloop *target;
+  callback * _Atomic notify;
 
-  struct bfd_session *session;
+  struct bfd_session *_Atomic session;
 
-  u8 state;
-  u8 diag;
-  u8 old_state;
-  u8 down;
+  struct bfd_state_pair {
+    struct bfd_state {
+      u8 state;
+      u8 diag;
+    } loc, rem;
+  } cur, old;
+
+  bool down;
 };
+
+void bfd_request_get_state(struct bfd_request *req);
+
+#include "lib/tlists.h"
 
 #define BGP_BFD_GRACEFUL	2	/* BFD down triggers graceful restart */
 
@@ -63,16 +81,17 @@ static inline struct bfd_options * bfd_new_options(void)
 
 #ifdef CONFIG_BFD
 
-struct bfd_request * bfd_request_session(pool *p, ip_addr addr, ip_addr local, struct iface *iface, struct iface *vrf, void (*hook)(struct bfd_request *), void *data, struct birdloop *target, const struct bfd_options *opts);
-void bfd_update_request(struct bfd_request *req, const struct bfd_options *opts);
+struct bfd_request_ref * bfd_request_session(pool *p, ip_addr addr, ip_addr local, struct iface *iface, struct iface *vrf, callback *notify, const struct bfd_options *opts);
+void bfd_update_request(struct bfd_request_ref *req, const struct bfd_options *opts);
+void bfd_request_update_state(struct bfd_request *req);
 
 static inline void cf_check_bfd(int use UNUSED) { }
 
 #else
 
-static inline struct bfd_request * bfd_request_session(pool *p UNUSED, ip_addr addr UNUSED, ip_addr local UNUSED, struct iface *iface UNUSED, struct iface *vrf UNUSED, void (*hook)(struct bfd_request *) UNUSED, void *data UNUSED, struct birdloop *target UNUSED, const struct bfd_options *opts UNUSED) { return NULL; }
-static inline void bfd_update_request(struct bfd_request *req UNUSED, const struct bfd_options *opts UNUSED) { };
-
+static inline struct bfd_request_ref * bfd_request_session(pool *p UNUSED, ip_addr addr UNUSED, ip_addr local UNUSED, struct iface *iface UNUSED, struct iface *vrf UNUSED, callback *notify UNUSED, const struct bfd_options *opts UNUSED) { return NULL; }
+static inline void bfd_update_request(struct bfd_request_ref *req UNUSED, const struct bfd_options *opts UNUSED) { };
+static inline void bfd_request_update_state(struct bfd_request *req UNUSED) { bug("BFD not compiled in!"); }
 static inline void cf_check_bfd(int use) { if (use) cf_error("BFD not available"); }
 
 #endif /* CONFIG_BFD */
