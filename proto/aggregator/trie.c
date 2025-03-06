6d4f35136e9516616802746f5cc2232a50ab7e88
Igor Putovny <igor.putovny@nic.cz>
2025-03-06 15:44:48 +0100

    Remove assertions
    If prefix node was pruned from the trie, these assertions may not hold
    for returned node

--- proto/aggregator/trie.c
+++ proto/aggregator/trie.c
@@ -960,10 +960,7 @@ aggregator_recalculate(struct aggregator_proto *p, struct aggregator_route *old,
     const u32 pxlen = net_pxlen(addr);
 
     updated_node = aggregator_trie_remove_prefix(p, prefix, pxlen);
-
     ASSERT_DIE(updated_node != NULL);
-    ASSERT_DIE(updated_node->px_origin == FILLER);
-    ASSERT_DIE(updated_node->original_bucket == NULL);
   }
   else /* Announce or update */
   {
