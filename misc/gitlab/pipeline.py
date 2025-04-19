2d6c2e5836a744954f3fd9075547c05cec6f4aee
Maria Matejka <mq@ucw.cz>
2025-04-19 22:38:49 +0200

    fixup! fixup! fixup! fixup! fixup! fixup

--- misc/gitlab/pipeline.py
+++ misc/gitlab/pipeline.py
@@ -21,19 +21,20 @@ except ModuleNotFoundError:
 # Load the actual template
 template = env.get_template(f'{sys.argv[1]}-template.yml.j2')
 
-# Stages are always the same
-print("""
-stages:
-  - consistency
-  - image
-  - build
-  - pkg
-  - test
-  - release
-""")
+# Print the pipeline header (if exists)
+try:
+    print(env.get_template('{sys.argv[1]}-header.yml.j2').render())
+except jinja2.exceptions.TemplateNotFound:
+    pass
 
 # Render the template
 for item in data:
     print(template.render(item))
     print()
     print()
+
+# Print the pipeline footer (if exists)
+try:
+    print(env.get_template('{sys.argv[1]}-footer.yml.j2').render())
+except jinja2.exceptions.TemplateNotFound:
+    pass
