3318ae83cd78d1e63c6ff32000152d943a0f8149
Maria Matejka <mq@ucw.cz>
2025-12-19 13:58:58 +0100

    do not set igp_metric in bgp

--- proto/bgp/packets.c
+++ proto/bgp/packets.c
@@ -1133,10 +1133,7 @@ bgp_apply_next_hop(struct bgp_parse_state *s, ea_list **to, ip_addr gw, ip_addr 
       WITHDRAW(BAD_NEXT_HOP " - address %I is local", nbr->addr);
 
     if (c->cf->cost)
-    {
       ea_set_attr_u32(to, &ea_gen_local_metric, 0, c->cf->cost);
-      ea_set_attr_u32(to, &ea_gen_igp_metric, 0, c->cf->cost);
-    }
 
     struct nexthop_adata_mpls nam;
     memset(&nam, 0, sizeof nam);
