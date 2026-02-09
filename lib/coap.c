6d821671a8d46a7b2d209d35715e00b8bfe5cc08
Maria Matejka <mq@ucw.cz>
2026-02-09 17:57:34 +0100

    CoAP: Rough implementation of wire format parser.

---
+++ lib/coap.c
@@ -0,0 +1,448 @@
+/*
+ *	CoAP -- Constrained Application Protocol
+ *
+ *	(c) 2026 CZ.NIC
+ *	(c) 2026 Maria Matejka <mq@jmq.cz>
+ *
+ *	Can be freely distributed and used under the terms of the GNU GPL.
+ */
+
+/**
+ * DOC: Constrained Application Protocol
+ *
+ * Constrained Application Protocol is a minimalist protocol described
+ * by RFC 7252 and several more. It's designed for constrained environments
+ * but we are using it for high-performance API.
+ *
+ * CoAP is somehow convertible from/to HTTP, as there are many equivalent
+ * constructions and principles.
+ *
+ * This implementation aims for step-by-step parsing. We wanna avoid allocating
+ * data while parsing, and instead of creating generic structures, we generate
+ * events and let the caller process parts of the message on-the-fly;
+ * most specifically, allocate and copy only these parts which really need to
+ * be parsed.
+ *
+ * Implemented standards:
+ * RFC 7252 -- The Constrained Application Protocol (CoAP) (rudimentary support for UDP)
+ * RFC 8323 -- CoAP over TCP, TLS and Websockets (only TCP for now)
+ */
+
+#include "lib/birdlib.h"
+#include "lib/coap.h"
+#include "lib/string.h"
+
+#include <limits.h>
+
+STATIC_ASSERT_MSG(CHAR_BIT == 8, "Weird char length");
+
+#define END			(ctx->data_ptr >= ctx->data_len)
+#define CUR			(&ctx->data[ctx->data_ptr])
+#define ONEBYTE()		(ctx->data[ctx->data_ptr++])
+#define REMAINS			(ctx->data_len - ctx->data_ptr)
+
+#define FAIL(_e) return (ctx->state = _e)
+
+#define EAT(_n)	do { \
+  ctx->data_ptr += (_n); \
+  if (ctx->data_len <= ctx->data_ptr) \
+    FAIL(COAP_PSE_TRUNCATED); \
+} while (0)
+
+#define COPY(_tgt, _n) do { \
+  const void *_cur = CUR; \
+  uint _nn = (_n); \
+  EAT(_nn); \
+  memcpy(_tgt, _cur, _nn); \
+} while (0)
+
+#define GET(_v) COPY(&_v, sizeof (_v))
+
+#define LOADINT(which, next_state)  do { \
+  ctx->which <<= 8; \
+  ctx->which += ONEBYTE(); \
+  if (!--ctx->load_len_missing) \
+  { \
+    switch (ctx->load_len) \
+    { \
+      case 1: ctx->which += 13;		/* fall through */ \
+      case 2: ctx->which += 256;	/* fall through */ \
+      case 4: ctx->which += 65536;	/* fall through */ \
+	      break; \
+      default: bug("Weird load len"); \
+    } \
+    ctx->state = next_state; \
+  } \
+} while (0)
+
+static enum coap_parse_state
+coap_parse_option(struct coap_session *s, bool allow_partial)
+{
+#define MORE(_s) \
+  if (!allow_partial && ( \
+	((_s) >= COAP_PSM_NONE) && \
+	((_s) < COAP_PSE_NONE) || \
+	((_s) == COAP_PS_OPTION_PARTIAL))) \
+    return ctx->state = (_s); \
+  else \
+    FAIL(COAP_PSE_TRUNCATED)
+
+  struct coap_parse_context *ctx = &s->parser;
+
+  while (!END)
+  {
+    switch (ctx->state)
+    {
+      case COAP_PS_EMPTY:
+      case COAP_PSM_NONE ... COAP_PSM_OPTION_NONE:
+      case COAP_PS__MORE_MAX:
+      case COAP_PS_PAYLOAD_PARTIAL:
+      case COAP_PS_PAYLOAD_COMPLETE:
+      case COAP_PS_ERROR:
+	bug("Unexpected state when parsing CoAP option");
+
+      case COAP_PS_OPTION_COMPLETE:
+      case COAP_PS_HEADER:
+      case COAP_PSM_OPTION_DL:
+	ctx->option_dlbyte = ONEBYTE();
+
+	switch (ctx->option_dlbyte >> 4)
+	{
+	  case 0xf:
+	    if (ctx->option_dlbyte != 0xff)
+	      FAIL(COAP_PSE_FAKE_PAYLOAD_MARKER);
+	    else
+	    {
+	      ctx->state = COAP_PS_PAYLOAD_PARTIAL;
+	      ctx->payload_chunk_offset = ctx->payload_chunk_len = 0;
+	      continue;
+	    }
+
+	  case 0xe:
+	  case 0xd:
+	    ctx->load_len = ctx->load_len_missing = 1 << ((ctx->option_dlbyte >> 4) - 13);
+	    ctx->state = COAP_PSM_OPTION_DELTA;
+
+	    if (END)
+	      MORE(ctx->state);
+
+	    break;
+
+	  default:
+	    ctx->option_delta = ctx->option_dlbyte >> 4;
+	    break;
+	}
+
+	/* We have parsed just one half of the dlbyte but now we may need to parse
+	 * the option delta before parsing the length */
+
+	/* fall through */
+
+      case COAP_PSM_OPTION_DELTA:
+	if (ctx->state == COAP_PSM_OPTION_DELTA)
+	{
+	  LOADINT(option_delta, COAP_PSM_OPTION_PRE_LEN);
+
+	  /* We have parsed a byte but we need more, continue */
+	  if (ctx->state == COAP_PSM_OPTION_DELTA)
+	    continue;
+	}
+
+	/* Now the option delta is parsed, continue to the length */
+
+	/* fall through */
+
+      case COAP_PSM_OPTION_PRE_LEN:
+	switch (ctx->option_dlbyte & 0xf)
+	{
+	  case 0xf:
+	    FAIL(COAP_PSE_RESERVED_OPTION_LEN);
+
+	  case 0xe:
+	  case 0xd:
+	    ctx->load_len = ctx->load_len_missing = 1 << ((ctx->option_dlbyte & 0xf) - 13);
+	    ctx->state = COAP_PSM_OPTION_LEN;
+	    break;
+
+	  default:
+	    ctx->option_len = ctx->option_dlbyte & 0xf;
+	    break;
+	}
+
+	if (END)
+	  MORE(ctx->state);
+
+	/* Now finally parsing the option len bytes */
+
+	/* fall through */
+
+      case COAP_PSM_OPTION_LEN:
+	if (ctx->state == COAP_PSM_OPTION_LEN)
+	{
+	  LOADINT(option_len, COAP_PS_OPTION_PARTIAL);
+
+	  /* We have parsed a byte but we need more, continue */
+	  if (ctx->state == COAP_PSM_OPTION_LEN)
+	    continue;
+	}
+
+	/* Zero-length option is already done */
+	if (ctx->option_len == 0)
+	  MORE(COAP_PS_OPTION_COMPLETE);
+
+	/* Now loading the option */
+	ctx->option_chunk_offset = 0;
+	ctx->option_chunk_len = 0;
+
+	/* fall through */
+
+      case COAP_PS_OPTION_PARTIAL:
+	if (END)
+	  MORE(ctx->state);
+
+	ctx->option_value = CUR;
+	ctx->option_chunk_offset += ctx->option_chunk_len;
+	ctx->option_chunk_len = ctx->data_len - ctx->data_ptr;
+	if (ctx->option_chunk_len + ctx->option_chunk_offset >= ctx->option_len)
+	{
+	  ctx->option_chunk_len = ctx->option_len - ctx->option_chunk_offset;
+	  ctx->data_ptr += ctx->option_chunk_len;
+	  ASSERT_DIE(ctx->data_ptr <= ctx->data_len);
+	  MORE(COAP_PS_OPTION_COMPLETE);
+	}
+	else
+	{
+	  ctx->data_ptr = ctx->data_len;
+	  MORE(COAP_PS_OPTION_PARTIAL);
+	}
+
+
+    }
+  }
+
+#undef MORE
+}
+
+/**
+ * coap_udp_parse - do one step in incoming CoAP message parsing
+ * @s: The CoAP session.
+ *
+ * The caller is expected to repeatedly call this function (e.g. in a
+ * |while|-loop) until it returns COAP_PS_PAYLOAD_COMPLETE.
+ *
+ * Needs to be fed by coap_udp_rx() before every packet.
+ */
+enum coap_parse_state
+coap_udp_parse(struct coap_session *s)
+{
+  struct coap_parse_context *ctx = &s->parser;
+
+  switch (ctx->state)
+  {
+    /* Refuse to continue if error happened, or called again
+     * with payload already complete; reset needed */
+    case COAP_PS_PAYLOAD_COMPLETE:
+    case COAP_PS_ERROR:
+      FAIL(COAP_PSE_NEED_RESET);
+
+    /* Nothing parsed yet, let's start with the message header
+     * as defined in RFC 7252 Sec. 3 */
+    case COAP_PS_EMPTY:
+      {
+	/* 2 bits version, 2 bits type, 4 bits token length. */
+	u8 vtk;
+	GET(vtk);
+
+	/* Version must be 1 */
+	if ((ctx->version = vtk >> 6) != 1)
+	  FAIL(COAP_PSE_INVALID_VERSION);
+
+	/* Message type; all values defined, no error */
+	ctx->type = (vtk >> 4) & 0x3;
+
+	/* Token lengths 9 to 15 reserved by RFC 7252 */
+	if ((ctx->token_len = vtk & 0xf) > 8)
+	  FAIL(COAP_PSE_INVALID_TOKLEN);
+
+	/* 3 bytes class, 5 bytes details */
+	GET(ctx->code);
+	ctx->class = ctx->code >> 5;
+
+	/* Load message id, that's just a blob */
+	GET(ctx->msg_id);
+
+	/* Consume the token size */
+	if (ctx->token_len)
+	  COPY(&ctx->token, ctx->token_len);
+
+	/* Header processed */
+	ctx->common_len = ctx->data_len - (4 + ctx->token_len);
+	if (ctx->common_len < 1)
+	  FAIL(COAP_PSE_TRUNCATED);
+
+	return (ctx->state = COAP_PS_HEADER);
+      }
+
+    /* Call common option parser */
+    case COAP_PS_HEADER:
+    case COAP_PS_OPTION_PARTIAL:
+    case COAP_PS_OPTION_COMPLETE:
+      return coap_parse_option(s, false);
+
+      /* fall through */
+    case COAP_PS_MORE:
+    case COAP_PS_PAYLOAD_PARTIAL:
+      /* If more data would be needed, the UDP packet has been truncated */
+      FAIL(COAP_PSE_TRUNCATED);
+  }
+
+  bug("Unimplemented");
+}
+
+/**
+ * coap_udp_rx - more data received
+ * @s: The CoAP session
+ * @ptr: Packet data
+ * @len: Packet length
+ *
+ * The UDP parser expects the whole packet to be received at once by the
+ * underlying layer. Resets the parser so that one may run coap_udp_parse()
+ * afterwards.
+ */
+void
+coap_udp_rx(struct coap_session *s, const char *data, uint len)
+{
+  struct coap_parse_context *ctx = &s->parser;
+
+  ctx->state = COAP_PS_EMPTY;
+  ctx->data = data;
+  ctx->data_len = len;
+  ctx->data_ptr = 0;
+}
+
+/**
+ * coap_tcp_parse - do one step in incoming CoAP message parsing
+ * @ctx: Context structure previously initialized by coap_parse_init()
+ *
+ * The caller is expected to repeatedly call this function (e.g. in a
+ * |while|-loop) until it returns COAP_PMS_PAYLOAD_COMPLETE.
+ *
+ * Returns true if there is still something remaining to parse. Check
+ * |ctx->state| to find out what has happened.
+ */
+enum coap_parse_state
+coap_tcp_parse(struct coap_session *s)
+{
+  struct coap_parse_context *ctx = &s->parser;
+
+  while (!END)
+  {
+    switch (ctx->state)
+    {
+      case COAP_PS_PAYLOAD_COMPLETE:
+      case COAP_PS_EMPTY:
+	{
+	  u8 lentkl = ONEBYTE();
+
+	  /* Token lengths 9 to 15 reserved by RFC 7252 */
+	  if ((ctx->token_len_missing = ctx->token_len = lentkl & 0xf) > 8)
+	    FAIL(COAP_PSE_INVALID_TOKLEN);
+
+	  u8 elen = lentkl >> 4;
+	  if (elen <= 12)
+	  {
+	    ctx->common_len = elen;
+	    ctx->state = COAP_PSM_CODE;
+	  }
+	  else
+	  {
+	    ctx->load_len_missing = ctx->load_len = (1 << (elen - 13));
+	    ctx->state = COAP_PSM_ELEN;
+	  }
+
+	  continue;
+	}
+
+      case COAP_PSM_ELEN:
+	LOADINT(common_len, COAP_PSM_CODE);
+	continue;
+
+      case COAP_PSM_CODE:
+	ctx->code = ONEBYTE();
+	ctx->class = ctx->code >> 5;
+
+	if (!ctx->token_len)
+	  return ctx->state = COAP_PS_HEADER;
+
+	ctx->state = COAP_PSM_TOKEN;
+	continue;
+
+      case COAP_PSM_TOKEN:
+	ctx->token[ctx->token_len - ctx->token_len_missing] = ONEBYTE();
+	if (!--ctx->token_len_missing)
+	  return ctx->state = COAP_PS_HEADER;
+
+	continue;
+
+      case COAP_PS_HEADER:
+      case COAP_PS_OPTION_PARTIAL:
+      case COAP_PS_OPTION_COMPLETE:
+      case COAP_PSM_OPTION_NONE ... COAP_PS__MORE_MAX:
+	return coap_parse_option(s, true);
+
+      case COAP_PS_PAYLOAD_PARTIAL:
+	ctx->payload = CUR;
+	ctx->payload_chunk_offset += ctx->payload_chunk_len;
+	ctx->payload_chunk_len = ctx->data_len - ctx->data_ptr;
+	if (ctx->payload_chunk_len + ctx->payload_chunk_offset >= ctx->payload_total_len)
+	{
+	  ctx->payload_chunk_len = ctx->payload_total_len - ctx->payload_chunk_offset;
+	  ctx->data_ptr += ctx->payload_chunk_len;
+	  ASSERT_DIE(ctx->data_ptr <= ctx->data_len);
+	  return ctx->state = COAP_PS_PAYLOAD_COMPLETE;
+	}
+	else
+	{
+	  ctx->data_ptr = ctx->data_len;
+	  return ctx->state = COAP_PS_PAYLOAD_PARTIAL;
+	}
+
+      case COAP_PSM_NONE:
+      case COAP_PS_ERROR:
+	FAIL(COAP_PSE_NEED_RESET);
+    }
+  }
+}
+
+
+#if 0
+static const char *
+coap_parse_strerror(const struct coap_parsed_message *ctx)
+{
+  switch (ctx->state) {
+    case COAP_PMSE_TRUNCATED:
+      return tmp_sprintf(
+	  "Truncated message (length %u, needed at least %u)",
+	  ctx->data_len, ctx->data_ptr);
+
+    case COAP_PMSE_INVALID_VERSION:
+      return tmp_sprintf("Invalid version: %u", ctx->version);
+
+    case COAP_PMSE_INVALID_TOKLEN:
+      return tmp_sprintf("Invalid token length: %u", ctx->token_len);
+
+    case COAP_PMS_EMPTY:
+      return "OK, initialized";
+
+    case COAP_PMS_HEADER:
+      return tmp_sprintf("OK, header parsed: version %u, type %u, token length %u",
+	  ctx->version,
+	  ctx->type, /* TODO: stringify */
+	  ctx->token_len);
+	  
+    default:
+      return tmp_sprintf("Unknown %s: %u",
+	  (ctx->state <= COAP_PMS_ERROR) ? "state" : "error", ctx->state);
+  }
+}
+#endif
---
+++ lib/coap.h
@@ -0,0 +1,195 @@
+/*
+ *	BIRD -- Minimalist CoAP Module for CoAP over reliable connections 
+ *
+ *	(c) 2026 CZ.NIC
+ *	(c) 2026 Maria Matejka <mq@jmq.cz>
+ *
+ *	Can be freely distributed and used under the terms of the GNU GPL.
+ */
+
+#ifndef _LIB_COAP_H_
+#define _LIB_COAP_H_
+
+#include "lib/birdlib.h"
+#include "lib/ip.h"
+
+/*
+ * RFC-specified constants
+ */
+
+/* RFC 7252: Message types for CoAP over UDP */
+enum coap_udp_msg_type {
+  COAP_TYPE_CONFIRMABLE = 0,
+  COAP_TYPE_NONCONFIRMABLE = 1,
+  COAP_TYPE_ACK = 2,
+  COAP_TYPE_RESET = 3,
+} PACKED;
+
+/* Message class; 3 bits */
+enum coap_msg_class {
+  COAP_CLS_REQUEST		= 0,		/* RFC 7252 */
+  COAP_CLS_RESPONSE		= 2 << 5,	/* RFC 7252 */
+  COAP_CLS_CLIENT_ERROR		= 4 << 5,	/* RFC 7252 */
+  COAP_CLS_SERVER_ERROR		= 5 << 5,	/* RFC 7252 */
+  COAP_CLS_STREAM_CONTROL	= 7 << 5,	/* RFC 8323 */
+  COAP_CLS__MASK		= 7 << 5,	/* Bitmask to extract just the class */
+} PACKED;
+
+/* Full message code for COAP_CLS_REQUEST */
+enum coap_msg_code {
+  COAP_REQ_EMPTY			= COAP_CLS_REQUEST | 0,		/* RFC 7252 */
+  COAP_REQ_GET				= COAP_CLS_REQUEST | 1,		/* RFC 7252 */
+  COAP_REQ_POST				= COAP_CLS_REQUEST | 2,		/* RFC 7252 */
+  COAP_REQ_PUT				= COAP_CLS_REQUEST | 3,		/* RFC 7252 */
+  COAP_REQ_DELETE			= COAP_CLS_REQUEST | 4,		/* RFC 7252 */
+
+  COAP_RESP_CREATED			= COAP_CLS_RESPONSE | 1,	/* RFC 7252 */
+  COAP_RESP_DELETED			= COAP_CLS_RESPONSE | 2,	/* RFC 7252 */
+  COAP_RESP_VALID			= COAP_CLS_RESPONSE | 3,	/* RFC 7252 */
+  COAP_RESP_CHANGED			= COAP_CLS_RESPONSE | 4,	/* RFC 7252 */
+  COAP_RESP_CONTENT			= COAP_CLS_RESPONSE | 5,	/* RFC 7252 */
+
+  COAP_CERR_BAD_REQUEST			= COAP_CLS_CLIENT_ERROR | 0,	/* RFC 7252 */
+  COAP_CERR_UNAUTHORIZED		= COAP_CLS_CLIENT_ERROR | 1,	/* RFC 7252 */
+  COAP_CERR_BAD_OPTION			= COAP_CLS_CLIENT_ERROR | 2,	/* RFC 7252 */
+  COAP_CERR_FORBIDDEN			= COAP_CLS_CLIENT_ERROR | 3,	/* RFC 7252 */
+  COAP_CERR_NOT_FOUND			= COAP_CLS_CLIENT_ERROR | 4,	/* RFC 7252 */
+  COAP_CERR_METHOD_NOT_ALLOWED		= COAP_CLS_CLIENT_ERROR | 5,	/* RFC 7252 */
+  COAP_CERR_NOT_ACCEPTABLE		= COAP_CLS_CLIENT_ERROR | 6,	/* RFC 7252 */
+  COAP_CERR_PRECONDITION_FAILED		= COAP_CLS_CLIENT_ERROR | 12,	/* RFC 7252 */
+  COAP_CERR_REQUEST_ENTITY_TOO_LARGE	= COAP_CLS_CLIENT_ERROR | 13,	/* RFC 7252 */
+  COAP_CERR_UNSUPPORTED_CONTENT_FORMAT	= COAP_CLS_CLIENT_ERROR | 15,	/* RFC 7252 */
+  COAP_CERR_IMA_TEAPOT			= COAP_CLS_CLIENT_ERROR | 18,	/* RFC 2324 per analogiam */
+
+  COAP_SERR_INTERNAL_SERVER_ERROR	= COAP_CLS_SERVER_ERROR | 0,	/* RFC 7252 */
+  COAP_SERR_NOT_IMPLEMENTED		= COAP_CLS_SERVER_ERROR | 1,	/* RFC 7252 */
+  COAP_SERR_BAD_GATEWAY			= COAP_CLS_SERVER_ERROR | 2,	/* RFC 7252 */
+  COAP_SERR_SERVICE_UNAVAILABLE		= COAP_CLS_SERVER_ERROR | 3,	/* RFC 7252 */
+  COAP_SERR_GATEWAY_TIMEOUT		= COAP_CLS_SERVER_ERROR | 4,	/* RFC 7252 */
+  COAP_SERR_PROXYING_NOT_SUPPORTED	= COAP_CLS_SERVER_ERROR | 5,	/* RFC 7252 */
+} PACKED;
+
+enum coap_parse_state {
+  COAP_PS_EMPTY = 0,			/* Nothing parsed */
+  COAP_PS_HEADER,			/* Header parsed */
+  COAP_PS_OPTION_PARTIAL,		/* Part of an option is available */
+  COAP_PS_OPTION_COMPLETE,		/* Option is complete */
+  COAP_PS_PAYLOAD_PARTIAL,		/* Part of the payload is available */
+  COAP_PS_PAYLOAD_COMPLETE,		/* Payload is complete */
+
+  COAP_PSM_NONE = 0x40,			/* More data needed (generic) */
+  COAP_PSM_CODE,			/* Missing code byte */
+  COAP_PSM_ELEN,			/* Missing extended length bytes */
+  COAP_PSM_TOKEN,			/* Missing token bytes */
+  COAP_PSM_OPTION_NONE,			/* Missing option bytes (generic) */
+  COAP_PSM_OPTION_DL,			/* Missing option DL byte */
+  COAP_PSM_OPTION_PRE_LEN,		/* Option delta loaded, length needed */
+  COAP_PSM_OPTION_DELTA,		/* Expected more option delta bytes */
+  COAP_PSM_OPTION_LEN,			/* Expected more option length bytes */
+  COAP_PS__MORE_MAX,
+
+#define COAP_PS_MORE COAP_PSM_NONE ... COAP_PS__MORE_MAX 
+
+  COAP_PSE_NONE = 0x80,			/* Error (generic) */
+  COAP_PSE_TRUNCATED,			/* Message too short */
+  COAP_PSE_INVALID_VERSION,		/* Unsupported CoAP version */
+  COAP_PSE_INVALID_TOKLEN,		/* Token too long: check token_len */
+  COAP_PSE_UNKNOWN_CLASS,		/* Message class not supported */
+  COAP_PSE_FAKE_PAYLOAD_MARKER,		/* Payload marker anticipation failed */
+  COAP_PSE_RESERVED_OPTION_LEN,		/* Reserved option length */
+  COAP_PSE_NEED_RESET,			/* Parser needs a reset */
+
+  COAP_PS__ERROR_MAX,
+#define COAP_PS_ERROR COAP_PSE_NONE ... COAP_PS__ERROR_MAX
+} PACKED;
+
+/* CoAP low-level parser context */
+struct coap_parse_context {
+  enum coap_parse_state state;	/* Parser state machine state */
+
+  enum coap_udp_msg_type type;	/* Type of the UDP message being parsed. UDP only. */
+  u16 msg_id;			/* UDP Message ID. */
+  u8 version;			/* CoAP version. Must be 1 by RFC 7252. UDP only. */
+
+  enum coap_msg_class class;	/* Message class from the code byte */
+  enum coap_msg_code code;	/* Full message code */
+
+  u8 token_len;			/* Token actual byte length (may be zero) */
+  u8 token_len_missing;		/* How many bytes we still need to load */
+  u8 token[8];			/* Token value, opaque bytes */
+
+  u8 option_dlbyte;		/* The DL byte of the option currently being loaded */
+
+  u32 load_len;			/* Byte length of a value being loaded */
+  u32 load_len_missing;		/* How many bytes we still need */
+
+  u64 common_len;		/* Message length minus transport-specific header length, minus already parsed options */
+
+  u32 option_delta;		/* Loaded option delta */
+  u32 option_type;		/* Current option type */
+  u32 option_len;		/* Current option value length */
+
+  u32 option_chunk_offset;	/* How far is the option chunk */
+  u32 option_chunk_len;		/* How long is the option chunk */
+  const char *option_value;	/* Current option value chunk */
+
+  u64 payload_chunk_offset;	/* How far is the chunk */
+  u64 payload_chunk_len;	/* How long is the chunk */
+  u64 payload_total_len;	/* Total chunk length; ~0 if unknown */
+  const char *payload;		/* Payload chunk data */
+
+  uint data_len;		/* Received data length */
+  uint data_ptr;		/* Where we are in the received data */
+  const char *data;		/* Received data to be parsed */
+};
+
+/* CoAP Endpoint identification */
+struct coap_endpoint_ident {
+  ip_addr remote;		/* UDP, TCP */
+  u16 port;			/* UDP, TCP */
+};
+
+#if 0
+/* Messages recently received */
+struct coap_seen_msg {
+  u16 msg_id;
+  enum coap_msg_type type;
+  u8 code;
+  btime expires;
+};
+#endif
+
+/* CoAP session and endpoint parameters */
+struct coap_params {
+  btime ack_timeout;		/* RFC 7252, Sec. 4.2: How long to wait for ACK */
+  u64 ack_random_factor;	/* RFC 7252, Sec. 4.2: Randomizer factor (* 2<<16) for ack_timeout */
+  btime default_leisure;	/* RFC 7252, Sec. 8.2: Default wait time limit for multicast responses */
+  uint max_retransmit;		/* RFC 7252, Sec. 4.2: Maximum number of retransmissions of confirmable msgs */
+  uint nstart;			/* RFC 7252, Sec. 4.7: Limit on outstanding messages */
+  uint probing_bps;		/* RFC 7252, Sec. 4.7: ??! */
+  uint max_endpoints;		/* Implementation: Maximum number of remote endpoints served at once */
+};
+
+
+/* One CoAP session */
+struct coap_session {
+  struct coap_endpoint *endpoint;	/* Local endpoint which we belong to */
+  struct coap_endpoint_ident remote;	/* Remote endpoint identification */
+  struct birdsock *sock;		/* Socket to act on */
+  struct coap_parse_context parser;	/* Frame parser */
+};
+
+/* Local CoAP endpoint, capable of connecting to multiple others */
+struct coap_endpoint {
+  struct coap_endpoint_ident ident;	/* Endpoint identification */
+// TLIST of sessions!  struct coap_ 
+  struct birdsock *sock;		/* Listening socket */
+};
+
+void coap_udp_rx(struct coap_session *s, const char *data, uint len);
+enum coap_parse_state coap_udp_parse(struct coap_session *s);
+
+void coap_tcp_rx(struct coap_session *s, const char *data, uint len);
+enum coap_parse_state coap_tcp_parse(struct coap_session *s);
+
+#endif /* _LIB_COAP_H_ */
