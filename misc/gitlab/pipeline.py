a36e6d6533167cfe7803fd686a9346a23a7cdb36
Maria Matejka <mq@ucw.cz>
2026-03-21 20:53:12 +0100

    CI: Split the makefile out, add documentation

--- misc/gitlab/pipeline.py
+++ misc/gitlab/pipeline.py
@@ -8,22 +8,22 @@ import sys
 import yaml
 
 # Find where we are
-localdir = pathlib.Path(__file__).parent
+_, template_file, data_file, *_ = sys.argv
 
 # Prepare Jinja2 environment
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(str(localdir)))
+env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
 env.filters.update({ "to_yaml": lambda x: "" if type(x) is jinja2.runtime.Undefined else yaml.dump(x).rstrip() })
 
 # Load and process input data
 try:
-    data = yaml.safe_load(rendered := env.get_template(f'data.yml.j2').render({}))
+    data = yaml.safe_load(rendered := env.get_template(data_file).render({}))
 except yaml.parser.ParserError as e:
     print("Failed to render input data, generated output here:")
     print(rendered)
     raise e
 
 # Load the actual template
-template = env.get_template(f'template.yml.j2')
+template = env.get_template(template_file)
 
 # Render the template
 final = template.render({ **data })
