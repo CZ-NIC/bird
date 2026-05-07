e3d8eabd2438979c06ecd6748d120e0d32e4e6eb
Maria Matejka <mq@ucw.cz>
2026-05-07 13:06:02 +0200

    BGP: Fix locking order violations in dynamic socket refusal
    In such improbable cases like failure to finish incoming connection
    setup, or a weird race condition with dynamic bgp shutdown, locking
    order was violated.
    
    None of these should be actually possible to ever happen but it's better
    to not have these there, before some LLM claims credits for that.
    
    Issue: #403

--- proto/bgp/bgp.c
+++ proto/bgp/bgp.c
@@ -1171,8 +1171,12 @@ bgp_down(struct bgp_proto *p)
 {
   /* Close the possibly unpicked dynamic BGP socket */
   if (p->postponed_sk)
-    BGP_LISTEN_LOCKED(bl)
-      sk_close(p->postponed_sk);
+  {
+    struct birdloop *loop = p->postponed_sk->loop;
+    birdloop_enter(loop);
+    sk_close(p->postponed_sk);
+    birdloop_leave(loop);
+  }
 
   p->postponed_sk = NULL;
 
@@ -2100,21 +2104,22 @@ bgp_incoming_connection_dynamic(struct callback *cb)
       ASSERT_DIE(sk);
       rem_node(&bis->n);
       mb_free(bis);
+
+      BGP_TRACE(D_EVENTS, "Incoming connection from %I%J (port %d) %s",
+	  sk->daddr, ipa_is_link_local(sk->daddr) ? sk->iface : NULL,
+	  sk->dport, "accepted");
+
+      /* Finish socket configuration */
+      if (!bgp_configure_incoming_socket(p, sk))
+	sk = NULL;
+
     }
-
-    BGP_TRACE(D_EVENTS, "Incoming connection from %I%J (port %d) %s",
-	sk->daddr, ipa_is_link_local(sk->daddr) ? sk->iface : NULL,
-	sk->dport, "accepted");
-
-    /* Finish socket configuration */
-    if (!bgp_configure_incoming_socket(p, sk))
-      continue;
-
 
     /* Ending up here means that there is no pre-existing explicit BGP session,
      * and therefore the socket was matched by a dynamic entry instead.
      * We need to spawn a new BGP session. */
-    bgp_spawn(p, sk);
+    if (sk)
+      bgp_spawn(p, sk);
   }
 }
 
