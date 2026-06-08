b1a6c44b6576c29ce536604a6417d5628152e330
Maria Matejka <mq@ucw.cz>
2026-06-08 15:22:29 +0200

    BGP: Fix parsing labelled NLRIs with no next hop
    Missing hostentry caused by missing next hop, i.e. on withdrawals or
    with malformed attribute sets, was causing a crash. Fixed the code to be
    more tolerant.
    
    Reported-By: BCIX
    Issue: #440

--- proto/bgp/packets.c
+++ proto/bgp/packets.c
@@ -1157,6 +1157,10 @@ bgp_apply_next_hop(struct bgp_parse_state *s, ea_list **to, ip_addr gw, ip_addr 
 static void
 bgp_apply_mpls_labels(struct bgp_parse_state *s, ea_list **to, u32 lnum, u32 labels[lnum])
 {
+  /* Ignore labels on withdraw */
+  if (!*to)
+    return;
+
   bgp_set_attr_data(to, BA_MPLS_LABEL_STACK, 0, labels, lnum * sizeof labels[0]);
 
   if (lnum > MPLS_MAX_LABEL_STACK)
@@ -1190,9 +1194,13 @@ bgp_apply_mpls_labels(struct bgp_parse_state *s, ea_list **to, u32 lnum, u32 lab
   else /* GW_RECURSIVE */
   {
     eattr *e = ea_find(*to, &ea_gen_hostentry);
-    ASSERT_DIE(e);
-    struct hostentry_adata *head = (void *) e->u.ptr;
-    ea_set_hostentry(to, head->he->tab, head->he->owner, head->he->addr, head->he->link, lnum, labels);
+    if (e)
+    {
+      struct hostentry_adata *head = (void *) e->u.ptr;
+      ea_set_hostentry(to, head->he->tab, head->he->owner, head->he->addr, head->he->link, lnum, labels);
+    }
+    else
+      REPORT("No hostentry for received MPLS labels");
   }
 }
 
