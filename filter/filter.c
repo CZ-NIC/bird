328a19ebb033b587223e347706186264e41ddea8
Ondrej Zajicek <santiago@crfreenet.org>
2026-07-02 17:21:50 +0200

    Filter: Fix zero arg handling
    memcpy() should not have NULL src arg
    
    Reported-By: lzx0xf1@gmail.com
    Target: patch

--- filter/filter.c
+++ filter/filter.c
@@ -131,7 +131,7 @@ interpret(struct filter_state *fs, const struct f_line *line, uint argc, const s
 
   /* Set the arguments and top-level variables */
   fstk->vcnt = line->vars + line->args;
-  memcpy(fstk->vstk, argv, sizeof(struct f_val) * line->args);
+  bmemcpy(fstk->vstk, argv, sizeof(struct f_val) * line->args);
   memset(fstk->vstk + argc, 0, sizeof(struct f_val) * line->vars);
 
   /* The same as with the value stack. Not resetting the stack completely for performance reasons. */
