dc0edaa01c116e4cc6546f3d21a09ecc01906a2f
Ondrej Zajicek <santiago@crfreenet.org>
2026-07-13 07:19:52 +0200

    BGP: Fix stack buffer overflow in Flowspec NLRI decoder
    Flowspec NLRI with zero length causes NLRI decoder to read beyond
    validated data, which may trigger a stack buffer overflow.
    
    Reported-By: Tobias Klein <tk@trapkit.de>
    Target: patch

--- proto/bgp/packets.c
+++ proto/bgp/packets.c
@@ -2115,7 +2115,7 @@ bgp_decode_nlri_flow4(struct bgp_parse_state *s, byte *pos, uint len, ea_list *a
     uint pxlen = 0;
 
     /* Decode dst prefix */
-    if (data[0] == FLOW_TYPE_DST_PREFIX)
+    if (dlen && (data[0] == FLOW_TYPE_DST_PREFIX))
     {
       px = flow_read_ip4_part(data);
       pxlen = flow_read_pxlen(data);
@@ -2203,7 +2203,7 @@ bgp_decode_nlri_flow6(struct bgp_parse_state *s, byte *pos, uint len, ea_list *a
     uint pxlen = 0;
 
     /* Decode dst prefix */
-    if (data[0] == FLOW_TYPE_DST_PREFIX)
+    if (dlen && (data[0] == FLOW_TYPE_DST_PREFIX))
     {
       px = flow_read_ip6_part(data);
       pxlen = flow_read_pxlen(data);
