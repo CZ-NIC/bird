8c8dd24c5cfee1fb9f16e8d22648172a33635bb3
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2026-08-25 11:57:59 +0200

    YANG: Draft YANG-CBOR interface

---
+++ yang/yang_cbor.c
@@ -0,0 +1,23 @@
+#include "yang/yang_cbor.h"
+
+bool
+yc_validate_inst_id(struct yang_inst_id *idd)
+{
+  return true;
+}
+
+void
+yc_compress_inst_id(struct yang_inst_id *idd)
+{
+}
+
+void
+yang_cbor_init(struct yang_session *se)
+{
+
+}
+
+struct yang_proc_state
+yang_cbor_rpc(struct yang_session *se)
+{
+}
--- yang/yang_cbor.h
+++ yang/yang_cbor.h
@@ -14,7 +14,40 @@
 
 #include "lib/birdlib.h"
 #include "nest/bird.h"
+#include "yang/yang.h"
+#include "yang/model.h"
 
 typedef int64_t sid_t;
+
+/* YANG instance-identifier */
+struct yang_inst_id {
+   sid_t sid;
+   struct yang_inst_id *parent;
+   void *keys;
+   void *value;
+   uint64_t index;
+};
+
+bool yc_validate_inst_id(struct yang_inst_id *iid);
+void yc_compress_inst_id(struct yang_inst_id *iid, struct yang_inst_id **compressed);
+
+void yang_cbor_init(struct yang_session *se);
+
+void yang_cbor_open_datastore(void);
+void yang_cbor_datastore_next_child(void);
+
+void yang_cbor_open_container(void);
+struct schema_node *yang_cbor_next_child(void);
+void yang_cbor_close_container(void);
+
+void yang_cbor_open_list(void);
+bool yang_cbor_has_next_element(void);
+void yang_cbor_close_list(void);
+
+void yang_cbor_open_leaf_list(void);
+/* yang_cbor_has_next_element() */
+void yang_cbor_close_list(void);
+
+void yang_cbor_value(void);
 
 #endif
